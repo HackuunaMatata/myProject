14be0aa4531aae6c5dad4b5113dce04825918150
Aleksandra Pankratova <aleksandra_pankratova@epam.com>
2017-05-23 00:29:28 +0300

    - add right brute force

--- src/server.js
+++ src/server.js
@@ -55,11 +55,19 @@ function tryCrashPassword(password, word) {
         return Promise.resolve('Вы не ввели пароль');
     }
     if (isFinite(password)) {
-        return bruteforce(password);
+        return bruteforceForInt(password);
     }
-    if (word === '') {
+    var chars = 0;
+    for (var i = 0; i < password.length; i++) {
+        if (('a' <= password[i]) && (password[i] <= 'z')) chars++;
+    }
+    if (chars === password.length) {
+        return bruteforceForChar(password);
+    }
+    return bruteforceForAll(password);
+    /*if (word === '') {
         return attackWithDictionary(password);
-    }
+    }*/
     return attackWithMask(password, word);
 }
 
@@ -85,7 +93,7 @@ function attackWithMask(password, word) {
     })
 }
 
-function bruteforce(password) {
+/*function bruteforceForInt(password) {
     return new Promise(function (resolve, reject) {
         var length = password.length;
         var time = 0;
@@ -112,7 +120,80 @@ function bruteforce(password) {
         console.log('eeeeeee');
         resolve(time);
     })
+}*/
+
+function bruteforceForInt(password) {
+    return new Promise(function (resolve, reject) {
+        var time = 0;
+        var start = Date.now();
+        bruteForce('0123456789', function (val) {
+            time = Date.now() - start;
+            return val === password || time > 300000;
+        });
+        time = (Date.now() - start) / 1000;
+        resolve(time);
+    })
 }
+
+function bruteforceForChar(password) {
+    return new Promise(function (resolve, reject) {
+        var time = 0;
+        var start = Date.now();
+        bruteForce('abcdefghijklmnopqrstuvwxyz', function (val) {
+            time = Date.now() - start;
+            return val === password || (Date.now() - start) > 300000;
+        });
+        time = (Date.now() - start) / 1000;
+        resolve(time);
+    })
+}
+
+function bruteforceForAll(password) {
+    return new Promise(function (resolve, reject) {
+        var time = 0;
+        var start = Date.now();
+        bruteForce('abcdefghijklmnopqrstuvwxyz0123456789!\\"#@`$%^&*()_-=+*/\';:.,?|<>[]{}~', function (val) {
+            time = Date.now() - start;
+            return val === password || (Date.now() - start) > 300000;
+        });
+        time = (Date.now() - start) / 1000;
+        resolve(time);
+    })
+}
+
+var bruteForce = function (characters, callback) {
+    var i, intToCharacterBasedString, result;
+    /*if(typeof characters === 'string') {*/
+     characters = characters.split("");
+     /*}
+     characters.sort(); // Sort all characters
+     characters = characters.filter(function(value, index, arr){ //удаляем повторы
+     if(index < 1) {
+     return true;
+     } else {
+     return value !== arr[index-1];
+     }
+     });*/
+    characters = [""].concat(characters); // Useless empty value to start this array on index = 1
+    intToCharacterBasedString = function (num) { // Annoying algorithm..
+        var charBasedString, modulo;
+        charBasedString = "";
+        while (num > 0) {
+            modulo = num % characters.length; // Basic calculating
+            charBasedString = characters[modulo] + charBasedString; // Just push it before the old characters
+            num = ((num - modulo) / characters.length); // New value of num, annoying calculation
+        }
+        return charBasedString;
+    };
+    i = 1;
+    while (i > 0) {
+        result = callback(intToCharacterBasedString(i));
+        if (result) { // If callbacks returns true: we did our job!
+            break;
+        }
+        i++;
+    }
+};
 
 server.listen(1110);
 console.info('Server started!');
